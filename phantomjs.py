d7a909cb5f4a012156c2d19fa827dd4bf9d2a63a
Hayden Knapp <haydenknapp@gmail.com>
2017-12-15 11:36:26 -0500

    fixed yml even though it was not broken?

--- phantomjs.py
+++ phantomjs.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
     driver = webdriver.PhantomJS()
@@ -10,18 +9,9 @@ if __name__ == "__main__":
     if ("No jobs matched your search.") in source:
         print("No jobs found!")
     else:
-        soup = BeautifulSoup(source, "html.parser")
-        jobtitles = soup.find_all("b")
-        jobinfo = soup.find_all("<td>")
         if (len(jobtitles) > 1):
             print("Information about the chemistry jobs.")
         else:
             print("Information about the chemistry job.")
 
-    curindex = 0
-    for i in range(int(len(jobtitles) / 2)):
-        jobtitles[i] = str(jobtitles[i])
-        print(jobtitles[i][jobtitles[i].index("\">") + 2:jobtitles[i].index("</a>")])
-    for i in range(len(jobinfo)):
-        print(jobinfo[i])
     driver.quit()
